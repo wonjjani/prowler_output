641b62919b77d5763250fa39441f6916fa8e2480
wonjjani <108343303+wonjjani@users.noreply.github.com>
2024-06-18 11:41:50 +0900

    add translater

--- index.py
+++ index.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import json
 from openpyxl import load_workbook
+# 셀 너비 자동 조정
 from openpyxl.utils import get_column_letter
+from deep_translator import GoogleTranslator
+import pyarrow
 
 # JSON 파일 읽기
-json_file_path = 'prowler-output-553153918398-20240610071217.ocsf.json'
+json_file_path = "C:/Users/eggra/Downloads/prowler_aws/prowler-output-553153918398-20240610071217.ocsf.json"
 with open(json_file_path, 'r') as file:
     data = json.load(file)
 
@@ -29,11 +32,21 @@ df.columns = [
     "AWS 감사 관리자 컨트롤 타워 가드레일 준수"
 ]
 
+# "상태 코드"가 "PASS"인 행을 제거
+df = df[df["상태 코드"] != "PASS"]
+
+# '상세 상태' 번역
+translator = GoogleTranslator(source='en', target='ko')
+df['상세 상태'] = df["상세 상태"].apply(lambda x: translator.translate(x) if pd.notnull(x) else x)
+df['위험 세부 정보'] = df["위험 세부 정보"].apply(lambda x: translator.translate(x) if pd.notnull(x) else x)
+df['설명'] = df["설명"].apply(lambda x: translator.translate(x) if pd.notnull(x) else x)
+df['조치 설명'] = df["조치 설명"].apply(lambda x: translator.translate(x) if pd.notnull(x) else x)
+
 # 제거할 컬럼 목록
 columns_to_remove = [
     "제품 이름", "제품 공급자 이름", "제품 버전", "버전", "카테고리", "의존", "관련", "메모", 
     "CIS-2.0 준수", "CIS-1.4 준수", "CIS-1.5 준수", "AWS 계정 보안 온보딩 준수", "생성 시간", 
-    "제품", "UID", "계정 이름", "계정 타입", "계정 타입 ID", "계정 UID", "계정 라벨", 
+    "제품 UID", "UID", "계정 이름", "계정 타입", "계정 타입 ID", "계정 UID", "계정 라벨", 
     "조직 이름", "조직 UID", "클라우드 제공자", "ENS-RD2022 준수", 
     "AWS 잘 설계된 프레임워크 보안 기둥 준수", "AWS 기본 보안 모범 사례 준수", 
     "FedRamp 중간 개정 4 준수", "NIST-CSF 1.1 준수", "ISO27001-2013 준수", "GDPR 준수", 
@@ -43,18 +56,16 @@ columns_to_remove = [
     "FedRAMP 낮은 개정 4 준수", "MITRE ATT&CK 준수", "AWS 기본 기술 리뷰 준수", 
     "AWS 감사 관리자 컨트롤 타워 가드레일 준수", "제품 UID", "타입 UID", "타입 이름", "이벤트 시간",
     "카테고리 이름", "카테고리 UID", "클래스 이름", "클래스 UID", "상태 ID", "활동 이름", "활동 ID",
-    
+    "제목"
 ]
 
 # 컬럼 제거
 df = df.drop(columns=columns_to_remove, errors='ignore')
 
-
 # 엑셀 파일로 저장
 output_file_path = 'prowler_hahaha.xlsx'
 df.to_excel(output_file_path, index=False)
 
-# 셀 너비 자동 조정
 wb = load_workbook(output_file_path)
 ws = wb.active
 
